e93d766389eefb444a7826537ec2d865e595b789
BillyLiao <hohey670@gmail.com>
2016-09-24 17:01:58 +0800

    Upload photos to parse

--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import Parse
 
 class TimelineViewController: UIViewController {
 
     var photoTakingHelper:  PhotoTakingHelper?
+    @IBOutlet weak var tableView: UITableView!
+    var posts: [Post] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,36 @@ class TimelineViewController: UIViewController {
         self.tabBarController?.delegate = self
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let followingQuery = PFQuery(className: "Follow")
+        followingQuery.whereKey("from", equalTo: PFUser.currentUser()!)
+        
+        let postsFromFollowedUsers = Post.query()
+        postsFromFollowedUsers!.whereKey("user", matchesKey: "toUser", inQuery: followingQuery)
+        
+        let postsFromThisUser = Post.query()
+        postsFromThisUser!.whereKey("user", equalTo: PFUser.currentUser()!)
+        
+        let query = PFQuery.orQueryWithSubqueries([postsFromFollowedUsers!, postsFromThisUser!])
+        query.includeKey("user")
+        query.orderByDescending("createdAt")
+        
+        query.findObjectsInBackgroundWithBlock { (result: [PFObject]?, error: NSError?) in
+            self.posts = result as? [Post] ?? []
+            
+            for post in self.posts {
+                do {
+                    let data = try post.imageFile?.getData()
+                    post.image = UIImage(data: data!, scale: 1.0)
+                }catch {
+                    print("could not load image")
+                }
+            }
+            self.tableView.reloadData()
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -26,7 +59,9 @@ class TimelineViewController: UIViewController {
     func takePhoto(){
         // instantiate photo taking class, provide callback for when photo is selected
         photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!, callback: { (image: UIImage?) in
-            print("received a callback")
+            let post = Post()
+            post.image = image
+            post.uploadPost()
         })
     }
 }
@@ -39,5 +74,19 @@ extension TimelineViewController: UITabBarControllerDelegate {
         }else {
             return true
         }
+    }
+}
+
+extension TimelineViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.posts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostTableViewCell
+        cell.postImageView.image = self.posts[indexPath.row].image
+        
+        return cell
     }
 }
