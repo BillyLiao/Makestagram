1b340345586b59904d65edb97359beed53c91568
BillyLiao <hohey670@gmail.com>
2016-09-24 17:01:10 +0800

    Fetching data from Parse and create custom postTableViewCell

---
+++ Makestagram/Views/Timeline/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Makestagram
+//
+//  Created by 廖慶麟 on 2016/9/24.
+//  Copyright © 2016年 Make School. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
